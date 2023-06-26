a147e4bc095063cd068af74f43f02ffb35a2a14b
flightlessbird <102932289+alexflightlessbird@users.noreply.github.com>
2023-06-26 18:24:48 -0400

    Setup functions for roundWinner and gameWinner

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,36 +1,40 @@
-// All code should be written in this file.
-
 // Global variable setup
-let playerOneMoveOneType;
-let playerOneMoveOneValue;
-let playerOneMoveTwoType;
-let playerOneMoveTwoValue;
-let playerOneMoveThreeType;
-let playerOneMoveThreeValue;
-let playerTwoMoveOneType;
-let playerTwoMoveOneValue;
-let playerTwoMoveTwoType;
-let playerTwoMoveTwoValue;
-let playerTwoMoveThreeType;
-let playerTwoMoveThreeValue;
+let playerOneMoveOneType,
+    playerOneMoveOneValue,
+    playerOneMoveTwoType,
+    playerOneMoveTwoValue,
+    playerOneMoveThreeType,
+    playerOneMoveThreeValue,
+    playerOneTotalWins,
+    playerTwoMoveOneType,
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoType,
+    playerTwoMoveTwoValue,
+    playerTwoMoveThreeType,
+    playerTwoMoveThreeValue,
+    playerTwoTotalWins;
 
 
-// Set variables based on input
+// Main function - set variables based on input
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
     if (!player || !moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue) {
-        return;
+        return null;
+        console.log("All values must be filled in.");
     }
 
     if (!validMoveType(moveOneType) || !validMoveType(moveTwoType) || !validMoveType(moveThreeType)) {
-        return;
+        return null;
+        console.log("One or more move types are not valid.");
     }
 
     if (!validMoveValue(moveOneValue) || !validMoveValue(moveTwoValue) || !validMoveValue(moveThreeValue)) {
-        return;
+        return null;
+        console.log("One or more move values are not valid.");
     }
 
     if (moveOneValue + moveTwoValue + moveThreeValue > 99) {
-        return;
+        return null;
+        console.log("The values cannot sum to more than 99.");
     }
 
     if (player ==="Player One") {
@@ -64,7 +68,7 @@ const validMoveValue = (moveValue) => {
     return  (moveValue >= 1) && (moveValue <= 99);
 }
 
-// Return appropriate winner for a specific round
+// Main function - return appropriate winner for a specific round
 const getRoundWinner = (round) => {
     switch(round) {
         case 1:
@@ -77,23 +81,98 @@ const getRoundWinner = (round) => {
             return (roundWinnerHelper(playerOneMoveThreeType, playerOneMoveThreeValue, playerTwoMoveThreeType, playerTwoMoveThreeValue));
             break;
         default:
-            return;
+            return null;
     }
 }
 
 // Helper function - determine winner of specific round
 const roundWinnerHelper = (playerOneType, playerOneValue, playerTwoType, playerTwoValue) => {
     if (!playerOneType || !playerOneValue || !playerTwoType || !playerTwoValue) {
-        return;
+        return null;
     }
 
     if (playerOneType === "rock") {
-
+        if (playerTwoType === "rock") {
+            return tieBreaker(playerOneValue, playerTwoValue);
+        } else if (playerTwoType === "paper") {
+            return "Player Two";
+        } else if (playerTwoType === "scissors") {
+            return "Player One";
+        } else {
+            return null;
+        }
     } else if (playerOneType === "paper") {
-
+        if (playerTwoType === "rock") {
+            return "Player One";
+        } else if (playerTwoType === "paper") {
+            return tieBreaker(playerOneValue, playerTwoValue);
+        } else if (playerTwoType === "scissors") {
+            return "Player Two";
+        } else {
+            return null;
+        }
     } else if (playerOneType === "scissors") {
+        if (playerTwoType === "rock") {
+            return "Player Two";
+        } else if (playerTwoType === "paper") {
+            return "Player One";
+        } else if (playerTwoType === "scissors") {
+            return tieBreaker(playerOneValue, playerTwoValue);
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
 
+// Helper function - determine winner when move types are the same
+const tieBreaker = (playerOneValue, playerTwoValue) => {
+    if (playerOneValue > playerTwoValue) {
+        return "Player One";
+    } else if (playerOneValue < playerTwoValue) {
+        return "Player Two";
+    } else if (playerOneValue == playerTwoValue) {
+        return "Tie";
+    } else {
+        return null;
+    }
+}
+
+// Main function - return appropriate winner after all three rounds
+const getGameWinner = () => {
+    if (!playerOneMoveOneType || !playerOneMoveOneValue || !playerOneMoveTwoType || !playerOneMoveTwoValue || !playerOneMoveThreeType || !playerOneMoveThreeValue || !playerTwoMoveOneType || !playerTwoMoveOneValue || !playerTwoMoveTwoType || !playerTwoMoveTwoValue || !playerTwoMoveThreeType || !playerTwoMoveThreeValue) {
+        return null;
+        console.log("One or more variables didn't set correctly.");
+    }
+
+    playerOneTotalWins = 0;
+    playerTwoTotalWins = 0;
+
+    const roundOneWinner = getRoundWinner(1);
+    const roundTwoWinner = getRoundWinner(2);
+    const roundThreeWinner = getRoundWinner(3);
+
+    winCalc(roundOneWinner);
+    winCalc(roundTwoWinner);
+    winCalc(roundThreeWinner);
+
+    if (playerOneTotalWins > playerTwoTotalWins) {
+        return "Player One";
+    } else if (playerOneTotalWins < playerTwoTotalWins) {
+        return "Player Two";
+    } else if (playerOneTotalWins == playerTwoTotalWins) {
+        return "Tie";
     } else {
         return;
+    }
+}
+
+// Helper function - add wins after specific round
+const winCalc = (winner) => {
+    if (winner === "Player One") {
+        playerOneTotalWins = playerOneTotalWins + 1;
+    } if (winner === "Player Two") {
+        playerTwoTotalWins = playerTwoTotalWins + 1;
     }
 }
